3ba36adfb4522212142866c1fc09962edd9fb8ac
Ziyaan Hassan <ziyaan2010@gmail.com>
2022-09-19 23:34:38 +0500

    page model doc blocks

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -5,6 +5,16 @@ namespace Z3d0X\FilamentFabricator\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property-read int $id
+ * @property-read string $title
+ * @property-read string $slug
+ * @property-read string $layout
+ * @property-read array $blocks
+ * @property-read int $parent_id
+ * @property-read \Illuminate\Support\Carbon $created_at
+ * @property-read \Illuminate\Support\Carbon $updated_at
+ */
 class Page extends Model
 {
     use HasFactory;
