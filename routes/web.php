61c21187546476b6d6737a028843495925b68d36
ZedoX <ziyaan2010@gmail.com>
2023-02-17 11:42:46 +0500

    refactor to page controller

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,14 @@
 <?php
 
 use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
-use Z3d0X\FilamentFabricator\Facades\FilamentFabricator;
-use Z3d0X\FilamentFabricator\Models\Contracts\Page;
+use Z3d0X\FilamentFabricator\Http\Controllers\PageController;
 
 if (config('filament-fabricator.routing.enabled')) {
     Route::middleware(SubstituteBindings::class)
         ->prefix(config('filament-fabricator.routing.prefix', null))
         ->group(function () {
-            Route::get('/{filamentFabricatorPage?}', function (?Page $filamentFabricatorPage = null) {
-                // Handle root (home) page
-                if (blank($filamentFabricatorPage)) {
-                    $pageUrls = FilamentFabricator::getPageUrls();
-
-                    $pageId = array_search('/', $pageUrls);
-
-                    $filamentFabricatorPage = FilamentFabricator::getPageModel()::query()
-                        ->where('id', $pageId)
-                        ->firstOrFail();
-                }
-
-                $layout = FilamentFabricator::getLayoutFromName($filamentFabricatorPage?->layout);
-
-                if (! isset($layout)) {
-                    throw new \Exception("Filament Fabricator: Layout \"{$filamentFabricatorPage->layout}\" not found");
-                }
-
-                $component = $layout::getComponent();
-
-                return Blade::render(
-                    <<<'BLADE'
-                    <x-dynamic-component
-                        :component="$component"
-                        :page="$page"
-                    />
-                    BLADE,
-                    ['component' => $component, 'page' => $filamentFabricatorPage]
-                );
-            })
+            Route::get('/{filamentFabricatorPage?}', PageController::class)
             ->where('filamentFabricatorPage', '.*')
             ->fallback();
         });
