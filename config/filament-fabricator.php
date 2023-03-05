6022002cd72f34df0029f2a3192e38fac898e79a
ZedoX <ziyaan2010@gmail.com>
2023-03-05 16:52:49 +0500

    fix: page middlewares

--- config/filament-fabricator.php
+++ config/filament-fabricator.php
@@ -23,6 +23,16 @@ return [
         ],
     ],
 
+    'middleware' => [
+        \Illuminate\Cookie\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        // \Illuminate\Session\Middleware\AuthenticateSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ],
+
     'page-model' => \Z3d0X\FilamentFabricator\Models\Page::class,
 
     'page-resource' => \Z3d0X\FilamentFabricator\Resources\PageResource::class,
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
-use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 use Z3d0X\FilamentFabricator\Facades\FilamentFabricator;
 use Z3d0X\FilamentFabricator\Http\Controllers\PageController;
 
 if (config('filament-fabricator.routing.enabled')) {
-    Route::middleware(SubstituteBindings::class)
+    Route::middleware(config('filament-fabricator.middleware') ?? [])
         ->prefix(FilamentFabricator::getRoutingPrefix())
         ->group(function () {
             Route::get('/{filamentFabricatorPage?}', PageController::class)
