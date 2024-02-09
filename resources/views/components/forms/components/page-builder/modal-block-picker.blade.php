733e5594ffaf9340e07901f83e696aab192375b4
ZedoX <75579178+Z3d0X@users.noreply.github.com>
2024-02-09 15:31:46 +0500

    [Feat]: Block Picker Styles (#136)
    * wip
    
    * fix modal style
    
    * Fix styling
    
    * ui fix
    
    * docs
    
    ---------
    
    Co-authored-by: Z3d0X <Z3d0X@users.noreply.github.com>

---
+++ resources/views/components/forms/components/page-builder/modal-block-picker.blade.php
@@ -0,0 +1,63 @@
+@props([
+    'action',
+    'afterItem' => null,
+    'blocks',
+    'columns' => null,
+    'statePath',
+    'trigger',
+    'width' => null,
+])
+
+<x-filament::modal
+    :width="$width"
+    {{ $attributes->class(['fi-fo-builder-block-picker']) }}
+>
+    <x-slot name="trigger">
+        <div class="flex justify-center w-full">
+            {{ $trigger }}
+        </div>
+    </x-slot>
+
+    <x-filament::grid
+        :default="$columns['default'] ?? 1"
+        :sm="$columns['sm'] ?? null"
+        :md="$columns['md'] ?? null"
+        :lg="$columns['lg'] ?? null"
+        :xl="$columns['xl'] ?? null"
+        :two-xl="$columns['2xl'] ?? null"
+        direction="column"
+    >
+        @foreach ($blocks as $block)
+            @php
+                $wireClickActionArguments = ['block' => $block->getName()];
+
+                if ($afterItem) {
+                    $wireClickActionArguments['afterItem'] = $afterItem;
+                }
+
+                $wireClickActionArguments = \Illuminate\Support\Js::from($wireClickActionArguments);
+
+                $wireClickAction = "mountFormComponentAction('{$statePath}', '{$action->getName()}', {$wireClickActionArguments})";
+            @endphp
+
+
+            <button
+                type="button"
+                class="flex flex-col items-center justify-center border border-gray-200 dark:border-white/10 w-full h-full gap-2 whitespace-nowrap rounded-md p-2 text-sm transition-colors duration-75 outline-none hover:bg-gray-50 focus-visible:bg-gray-50 dark:hover:bg-white/5 dark:focus-visible:bg-white/5"
+                x-on:click="close"
+                wire:click="{{ $wireClickAction }}"
+            >
+                @if ($icon = $block->getIcon())
+                    <x-filament::icon
+                        :icon="$icon"
+                        class="h-10 w-10 text-gray-400 dark:text-gray-500"
+                    />
+                @endif
+                <div>
+                    {{ $block->getLabel() }}
+                </div>
+            </button>
+        @endforeach
+    </x-filament::grid>
+</x-filament::modal>
+
--- src/Forms/Components/PageBuilder.php
+++ src/Forms/Components/PageBuilder.php
@@ -3,10 +3,16 @@
 namespace Z3d0X\FilamentFabricator\Forms\Components;
 
 use Filament\Forms\Components\Builder;
+use Z3d0X\FilamentFabricator\Enums\BlockPickerStyle;
 use Z3d0X\FilamentFabricator\Facades\FilamentFabricator;
+use Z3d0X\FilamentFabricator\FilamentFabricatorPlugin;
 
 class PageBuilder extends Builder
 {
+    protected string $view = 'filament-fabricator::components.forms.components.page-builder';
+
+    protected BlockPickerStyle $blockPickerStyle = BlockPickerStyle::Dropdown;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -25,5 +31,27 @@ class PageBuilder extends Builder
                 ->values()
                 ->toArray();
         });
+
+        $blockPickerStyle = FilamentFabricatorPlugin::get()->getBlockPickerStyle();
+
+        if (! is_null($blockPickerStyle)) {
+            $this->blockPickerStyle($blockPickerStyle);
+        }
+    }
+
+    public function blockPickerStyle(BlockPickerStyle $style): static
+    {
+        if ($style === BlockPickerStyle::Modal) {
+            $this->blockPickerColumns(3);
+        }
+
+        $this->blockPickerStyle = $style;
+
+        return $this;
+    }
+
+    public function getBlockPickerStyle(): BlockPickerStyle
+    {
+        return $this->blockPickerStyle;
     }
 }
