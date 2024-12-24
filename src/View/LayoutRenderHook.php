6f0ffda3321bfba749ffb12300361565df7dbd61
Voltra <Voltra@users.noreply.github.com>
2024-12-24 16:38:38 +0000

    Define constants for render hooks in place of hard-coded strings for autocompletion

---
+++ src/View/LayoutRenderHook.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Z3d0X\FilamentFabricator\View;
+
+class LayoutRenderHook
+{
+    const HEAD_START = 'filament-fabricator::head.start';
+
+    const HEAD_END = 'filament-fabricator::head.end';
+
+    const BODY_START = 'filament-fabricator::body.start';
+
+    const SCRIPTS_START = 'filament-fabricator::scripts.start';
+
+    const SCRIPTS_END = 'filament-fabricator::scripts.end';
+
+    const BODY_END = 'filament-fabricator::body.end';
+}
