a8b8a06cafa946d5850c8a229607277f733d69bd
AzizEmir <azizemir@tutanota.com>
2025-01-03 11:58:53 +0300

    feat ( Localization ) : add Turkish lang.

---
+++ resources/lang/tr/page-resource.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'labels' => [
+        'blocks' => 'Bloklar',
+        'layout' => 'Düzen',
+        'page' => 'Sayfa',
+        'pages' => 'Sayfalar',
+        'parent' => 'Üst',
+        'slug' => 'Kısa Ad',
+        'title' => 'Başlık',
+        'url' => 'URL',
+    ],
+
+    'errors' => [
+        'slug_starts_or_ends_with_slash' => 'Kısa ad bir eğik çizgi ile başlayamaz veya bitirilemez.',
+    ],
+
+    'actions' => [
+        'save' => 'Kaydet',
+        'visit' => 'Siteyi Aç',
+    ],
+];
