f598d9d8e6e8c7d7e278944a0a07bc859d6f5317
Voltra <Voltra@users.noreply.github.com>
2025-01-03 09:53:42 +0000

    Define constants for schema slots in place of hard-coded strings for autocompletion

---
+++ src/View/ResourcheSchemaSlot.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Z3d0X\FilamentFabricator\View;
+
+class ResourceSchemaSlot
+{
+    const BLOCKS_BEFORE = 'blocks.before';
+
+    const BLOCKS_AFTER = 'blocks.after';
+
+    const SIDEBAR_BEFORE = 'sidebar.before';
+
+    const SIDEBAR_AFTER = 'sidebar.after';
+}
