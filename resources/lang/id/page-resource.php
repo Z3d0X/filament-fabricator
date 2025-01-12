8452189af64810d62db562c6c6affa8e19657ca9
Wildan <cuinc99@gmail.com>
2025-01-12 15:42:01 +0800

    Add Indonesian language translations for page resource

---
+++ resources/lang/id/page-resource.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'labels' => [
+        'blocks' => 'Blok',
+        'layout' => 'Tata Letak',
+        'page' => 'Halaman',
+        'pages' => 'Halaman-halaman',
+        'parent' => 'Induk',
+        'slug' => 'Slug',
+        'title' => 'Judul',
+        'url' => 'URL',
+    ],
+
+    'errors' => [
+        'slug_starts_or_ends_with_slash' => 'Slug tidak boleh diawali atau diakhiri dengan garis miring.',
+    ],
+
+    'actions' => [
+        'save' => 'Simpan',
+        'visit' => 'Kunjungi',
+    ],
+];
