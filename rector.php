1a7fb68bff30893a81f2a13f333178e3aa150867
Voltra <25990549+Voltra@users.noreply.github.com>
2025-09-10 15:38:29 +0000

    Minor tweaks

---
+++ rector.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Set\LaravelSetProvider;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/bootstrap',
+        __DIR__.'/config',
+        __DIR__.'/routes',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withCache()
+    ->withParallel()
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class,
+        \Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector::class,
+    ])
+    ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        earlyReturn: true,
+    )
+    ->withSetProviders(LaravelSetProvider::class)
+    ->withComposerBased(laravel: true, phpunit: true)
+    ->withSets([
+        LaravelLevelSetList::UP_TO_LARAVEL_110,
+    ]);
