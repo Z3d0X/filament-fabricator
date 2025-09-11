ba94b82cb075834f02b59359b8253cdef95e5bc5
Voltra <25990549+Voltra@users.noreply.github.com>
2025-09-11 10:08:15 +0000

    Remove false-positives from rector rulesets

--- rector.php
+++ rector.php
@@ -2,10 +2,17 @@
 
 declare(strict_types=1);
 
-use Rector\Config\RectorConfig;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 use RectorLaravel\Set\LaravelLevelSetList;
 use RectorLaravel\Set\LaravelSetProvider;
+use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
+use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
+use Rector\DeadCode\Rector\Stmt\RemoveUnreachableStatementRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -18,9 +25,18 @@ return RectorConfig::configure()
     ->withCache()
     ->withParallel()
     ->withRules([
+        AddOverrideAttributeToOverriddenMethodsRector::class,
         TypedPropertyFromStrictConstructorRector::class,
-        \Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector::class,
     ])
+	->withSkip([
+		// Only add classes that give false positives here
+		ClosureToArrowFunctionRector::class,
+		DisallowedEmptyRuleFixerRector::class,
+		RemoveAlwaysTrueIfConditionRector::class,
+		RemoveUnreachableStatementRector::class,
+		SimplifyEmptyCheckOnEmptyArrayRector::class,
+		SimplifyUselessVariableRector::class,
+	])
     ->withPhpSets()
     ->withPreparedSets(
         deadCode: true,
