f55ffa196624a7cbf0a91c0d0e94ca1f07fa175c
Voltra <25990549+Voltra@users.noreply.github.com>
2025-09-16 22:14:03 +0000

    Minor tweaks

--- src/View/LayoutRenderHook.php
+++ src/View/LayoutRenderHook.php
@@ -4,15 +4,15 @@ namespace Z3d0X\FilamentFabricator\View;
 
 class LayoutRenderHook
 {
-    const HEAD_START = 'filament-fabricator::head.start';
+    public const HEAD_START = 'filament-fabricator::head.start';
 
-    const HEAD_END = 'filament-fabricator::head.end';
+    public const HEAD_END = 'filament-fabricator::head.end';
 
-    const BODY_START = 'filament-fabricator::body.start';
+    public const BODY_START = 'filament-fabricator::body.start';
 
-    const SCRIPTS_START = 'filament-fabricator::scripts.start';
+    public const SCRIPTS_START = 'filament-fabricator::scripts.start';
 
-    const SCRIPTS_END = 'filament-fabricator::scripts.end';
+    public const SCRIPTS_END = 'filament-fabricator::scripts.end';
 
-    const BODY_END = 'filament-fabricator::body.end';
+    public const BODY_END = 'filament-fabricator::body.end';
 }
--- src/View/ResourceSchemaSlot.php
+++ src/View/ResourceSchemaSlot.php
@@ -4,11 +4,11 @@ namespace Z3d0X\FilamentFabricator\View;
 
 class ResourceSchemaSlot
 {
-    const BLOCKS_BEFORE = 'blocks.before';
+    public const BLOCKS_BEFORE = 'blocks.before';
 
-    const BLOCKS_AFTER = 'blocks.after';
+    public const BLOCKS_AFTER = 'blocks.after';
 
-    const SIDEBAR_BEFORE = 'sidebar.before';
+    public const SIDEBAR_BEFORE = 'sidebar.before';
 
-    const SIDEBAR_AFTER = 'sidebar.after';
+    public const SIDEBAR_AFTER = 'sidebar.after';
 }
