86d11218077d3dd3147f8b845d48cc815647633e
Daniel Kamiński <daniel.kaminski123@gmail.com>
2025-10-13 23:30:43 +0200

    Add polish translations

---
+++ resources/lang/pl/page-resource.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'labels' => [
+        'blocks' => 'Bloki',
+        'layout' => 'Układ',
+        'page' => 'Strona',
+        'pages' => 'Strony',
+        'parent' => 'Element nadrzędny',
+        'slug' => 'Slug',
+        'title' => 'Tytuł',
+        'url' => 'URL',
+    ],
+
+    'errors' => [
+        'slug_starts_or_ends_with_slash' => 'Slug nie może zaczynać się ani kończyć znakiem „/”.',
+    ],
+
+    'actions' => [
+        'save' => 'Zapisz',
+        'visit' => 'Odwiedź',
+    ],
+];
