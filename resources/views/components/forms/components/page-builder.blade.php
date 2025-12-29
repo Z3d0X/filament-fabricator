46ae0891c90556cd1ed9684fe58690f1efe1253e
Voltra <25990549+Voltra@users.noreply.github.com>
2025-12-29 20:30:21 +0000

    Fix form action mounting in the page builder

--- resources/views/components/forms/components/page-builder.blade.php
+++ resources/views/components/forms/components/page-builder.blade.php
@@ -70,7 +70,7 @@
             <ul
                 x-sortable
                 data-sortable-animation-duration="{{ $getReorderAnimationDuration() }}"
-                wire:end.stop="{{ 'mountFormComponentAction(\'' . $statePath . '\', \'reorder\', { items: $event.target.sortable.toArray() })' }}"
+                wire:end.stop="{{ 'mountAction(\'reorder\', { items: $event.target.sortable.toArray() }, { schemaComponent: \'' . $key . '\' })' }}"
                 class="space-y-4"
             >
                 @php
